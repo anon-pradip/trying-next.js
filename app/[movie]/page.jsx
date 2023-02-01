7affc6031f72a33aa8123aabaa808bfdb4f976e0
anon-pradip <pradipsubedi360@gmail.com>
2023-02-01 15:26:48 +0545

    removed static generation

--- app/[movie]/page.jsx
+++ app/[movie]/page.jsx
@@ -1,17 +1,6 @@
 import Image from "next/image";
 
-export async function generateStaticParams() {
-  const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`);
-
-  const res = await data.json();
-
-  return res.results.map((movie) => ({
-    movie: toString(movie.id)
-  }))
-}
-
 const MovieDetail = async ({ params }) => {
-
   const imagePath = "https://image.tmdb.org/t/p/original"
   const { movie } = params;
   const data = await fetch(`https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`)
@@ -28,7 +17,6 @@ const MovieDetail = async ({ params }) => {
         <h2>Runtime: {res.runtime} minutes</h2>
         <h2 className="text-sm bg-green-700 rounded-sm px-4 py-2 my-2 inline-block mb-4">{res.status}</h2>
         <p className="text-slate-400 border-t py-4 border-slate-700 max-w-md text-justify">{res.overview}</p>
-
       </div>
     </>
   )
